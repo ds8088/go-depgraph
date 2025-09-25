26255e9e4d718754247c66dc5de0e7345c9865ee
ds8088 <212849436+ds8088@users.noreply.github.com>
2025-09-25 10:07:28 +0300

    chore: format examples with gofumpt

--- examples/iterator/iterator.go
+++ examples/iterator/iterator.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 
-    "github.com/ds8088/go-depgraph"
+	"github.com/ds8088/go-depgraph"
 )
 
 func main() {
-    dg := depgraph.NewDependencyGraph[string]()
-    dg.Add("A")
-    dg.Add("B", "A")
-    dg.Add("C")
+	dg := depgraph.NewDependencyGraph[string]()
+	dg.Add("A")
+	dg.Add("B", "A")
+	dg.Add("C")
 
-    // ResolveIter returns an Option-like pair of elements.
-    // Either `err` is nil, in which case `el` is valid,
-    // or `err` is non-nil, and if so, we should handle the error.
-    for el, err := range dg.ResolveIter() {
-        if err != nil {
-            panic(fmt.Errorf("resolving dependency graph: %w", err))
-        }
+	// ResolveIter returns an Option-like pair of elements.
+	// Either `err` is nil, in which case `el` is valid,
+	// or `err` is non-nil, and if so, we should handle the error.
+	for el, err := range dg.ResolveIter() {
+		if err != nil {
+			panic(fmt.Errorf("resolving dependency graph: %w", err))
+		}
 
-        // Should consecutively print "A", "C" and "B".
-        fmt.Println(el)
-    }
+		// Should consecutively print "A", "C" and "B".
+		fmt.Println(el)
+	}
 }
